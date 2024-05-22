f4c2821586c5d3b90292640985ea0685ca789620
Rodi1-PC <sortovs@yandex.ru>
2024-05-22 22:55:19 +0300

    added swagger

---
+++ src/main/java/com/rodi1/androidbackend/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.rodi1.androidbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rodi1.androidbackend.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
